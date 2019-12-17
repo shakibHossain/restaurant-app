773d3b35bb0fedb3832ab3f91f59ba85e6caf904
shakibHossain <shakib.hossain.sh@gmail.com>
2019-12-17 00:29:34 -0600

    Fixed axios error

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://api.yelp.com/v3/businesses',
 	headers: {
-		Authorization: 'Bearer ondgyd0UJUTOxsRhwF-G9OVNXvt9agxU9DDq5MVhpF3REtmqvxrL3NjI9FNA9xsvZyNvM0wcxBksZKDnU2z_9QZ0OhJRr2jps65d72IVJIELj-QABldXT0K-U-PoXXYx'
+		Authorization: 'Bearer xFuqkz18bCydiLbnCQjtrIUseP4BaOWj9_Ucn31AutFTbOqtkVQPO8suttqbjI8zx9FzvKWWwWXEbADEBd56Dbiy7aUkktG50-Gl_fEqCJNkO0dV5iHRCqPZ33D4XXYx'
 	}
 });
